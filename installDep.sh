6a5282aba14e6a268d46bd6a82c6c2d8353f2b85
JerryMouse <jiangzhangsg@qq.com>
2020-11-19 23:37:45 +0800

    cross compile lock free stack

--- installDep.sh
+++ installDep.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install build-essential python python3 pkg-config git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
+sudo apt install build-essential python python3 pkg-config git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev gcc-arm-linux-gnueabihf
