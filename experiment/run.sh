4e3805c5bc9ada74858cb18bd3d827933ca9c9fb
Tr0py <troppingz@gmail.com>
2020-12-12 14:17:58 +0800

    Add run info to scripts

--- experiment/run.sh
+++ experiment/run.sh
@@ -1,12 +1,16 @@
 mkdir -p logs/$1
 rm logs/$1/*.txt
+echo "Solution: $1"
 for program in blackscholes bodytrack facesim fluidanimate freqmine swaptions x264
 do
-	for thread in 1 2 4 8 16 32
-	do
-		for count in 1 2 3
-		do
-			parsecmgmt -a run -p $program -i simlarge -n $thread -s "time $(pwd)/../bin/$1" >> logs/$1/$program$thread.txt
-		done
-	done
+        echo "Program: $program"
+        for thread in 1 2 4 8 16 32
+        do
+                echo "Thread: $thread"
+                for count in 1 2 3
+                do
+                        echo "Count: $count"
+                        parsecmgmt -a run -p $program -i simlarge -n $thread -s "time $(pwd)/../bin/$1" >> logs/$1/$program$thread.txt
+                done
+        done
 done
